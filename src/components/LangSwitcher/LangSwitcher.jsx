296855ff4fcfc449cb09ee60df133371d4b1168e
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2023-02-13 15:38:51 +0200

    15_38

--- src/components/LangSwitcher/LangSwitcher.jsx
+++ src/components/LangSwitcher/LangSwitcher.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import css from './LangSwitcher.module.css';
+import uaFlag from '..//../images/uk.png';
+import enFlag from '..//../images/en.png';
+import ruFlag from '..//../images/ru.png';
 
 const languageOptions = [
-  { value: 'uk', label: 'Ukrainian' },
-  { value: 'en', label: 'English' },
-  { value: 'ru', label: 'Russian' },
+  { value: 'uk', label: 'Ukrainian', flag: uaFlag },
+  { value: 'en', label: 'English', flag: enFlag },
+  { value: 'ru', label: 'Russian', flag: ruFlag },
 ];
 
 export const LanguageSwitcher = () => {
   const [selectedLanguage, setSelectedLanguage] = useState('en');
+  const [selectedFlag, setSelectedFlag] = useState(enFlag);
 
   const handleLanguageChange = e => {
     setSelectedLanguage(e.target.value);
+    setSelectedFlag(e.target.flag);
   };
 
   return (
@@ -21,11 +26,21 @@ export const LanguageSwitcher = () => {
         value={selectedLanguage}
         onChange={handleLanguageChange}
       >
-        {languageOptions.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
+        {languageOptions.map(option => {
+          return (
+            <option
+              className={css.optionList}
+              key={option.value}
+              value={option.value}
+            >
+              {console.log(ruFlag)}
+              <div>
+                <img src={option.flag} alt={option.label} />
+              </div>
+              <div>{option.label}</div>
+            </option>
+          );
+        })}
       </select>
     </div>
   );
